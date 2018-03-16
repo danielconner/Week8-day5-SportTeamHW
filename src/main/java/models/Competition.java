ece8dfbbe2432d832cc1fcc9abf7d535558e8dc3
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-16 11:32:28 +0000

    added attributes to comp and team

--- src/main/java/models/Competition.java
+++ src/main/java/models/Competition.java
@@ -1,4 +1,53 @@
 package models;
 
+import java.util.Set;
+
 public class Competition {
+
+    private int id;
+    private String name;
+    private String prize;
+    private Set<Team> teams;
+
+    public Competition() {
+    }
+
+    public Competition(String name, String prize) {
+        this.name = name;
+        this.prize = prize;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrize() {
+        return prize;
+    }
+
+    public void setPrize(String prize) {
+        this.prize = prize;
+    }
+
+    public Set<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Set<Team> teams) {
+        this.teams = teams;
+    }
 }
+
+
--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,81 @@
 package models;
 
+import java.util.Set;
+
 public class Team {
+
+    private int id;
+    private String name;
+    private Manager manager;
+    private Director director;
+    private String colours;
+    private Boolean inComp;
+    private Set<Player> players;
+
+    public Team() {
+    }
+
+    public Team(String name, Manager manager, Director director, String colours, Boolean inComp) {
+        this.name = name;
+        this.manager = manager;
+        this.director = director;
+        this.colours = colours;
+        this.inComp = inComp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public String getColours() {
+        return colours;
+    }
+
+    public void setColours(String colours) {
+        this.colours = colours;
+    }
+
+    public Boolean getInComp() {
+        return inComp;
+    }
+
+    public void setInComp(Boolean inComp) {
+        this.inComp = inComp;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
 }
